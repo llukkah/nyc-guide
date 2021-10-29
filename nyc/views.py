de9428ddc634627299f523fc9a972a0bed4062c4
llukkah <llukkah.rey@gmail.com>
2021-10-29 19:21:49 -0400

    fixed dynamic urls

--- nyc/views.py
+++ nyc/views.py
@@ -9,13 +9,13 @@ def borough(request, borough):
     if request.method == 'GET':
         return render(request=request, template_name='borough.html', context={ 'borough': borough, 'activities': boroughs[borough].keys() })
 
-# Create your views here.
-def activity(request, borough, activity):
-    # pass
-    if request.method == 'GET':
-        return render(request=request, template_name='activity.html', context={'activity': activity, 'activities': boroughs[borough][activity].keys()})
-
 def venue(request, borough, activity, venue):
     # pass
     if request.method == 'GET':
         return render(request=request, template_name='venue.html')
+
+# Create your views here.
+def activity(request, borough, activity):
+    # pass
+    if request.method == 'GET':
+        return render(request=request, template_name='activity.html', context={'borough': borough, 'activity': activity, 'activities': boroughs[borough][activity].keys()})
